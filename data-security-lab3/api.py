6544ffdabdfd3f6bd19f8ba913ecd71aa89eca41
ytalashko <talashkoy@gmail.com>
2015-10-24 20:31:35 +0300

    Remove unnecessary staff; Vitalya do not cry

--- data-security-lab3/api.py
+++ data-security-lab3/api.py
@@ -6,26 +6,15 @@ from django.http import HttpResponse
 from django.views.decorators.csrf import csrf_exempt
 
 fs = FileSystem()
+active = False
 x = random.randint(1, 9)
-
-
-class JsonHttpResponse(HttpResponse):
-    def __init__(self, content=b'', type='primitive'):
-        super(JsonHttpResponse, self).__init__(JsonHttpResponse.to_json(content, type))
-
-    @staticmethod
-    def to_json(object, type):
-        if type == 'object':
-            return json.dumps(object.__dict__)
-        else:
-            return json.dumps(object)
 
 
 @csrf_exempt
 def get_x(request):
     global x
     x = random.randint(1, 9)
-    return JsonHttpResponse(x)
+    return HttpResponse(json.dumps(x))
 
 
 @csrf_exempt
@@ -34,19 +23,19 @@ def login(request):
     password = request.POST.get('password')
     y = request.POST.get('y')
     result = fs.login(user_name, password, x, y)
-    return JsonHttpResponse(result)
+    return HttpResponse(json.dumps(result))
 
 
 @csrf_exempt
 def logged(request):
     result = fs.logged()
-    return JsonHttpResponse(result)
+    return HttpResponse(json.dumps(result))
 
 
 @csrf_exempt
 def logout(request):
     result = fs.logout()
-    return JsonHttpResponse(result)
+    return HttpResponse(json.dumps(result))
 
 
 @csrf_exempt
@@ -54,30 +43,30 @@ def check_captcha(request):
     x = request.POST.get('x')
     y = request.POST.get('y')
     result = fs.check_captcha(x, y)
-    return JsonHttpResponse(result)
+    return HttpResponse(json.dumps(result))
 
 
 @csrf_exempt
 def read(request, path):
     result = fs.read(path)
-    return JsonHttpResponse(result, 'object')
+    return HttpResponse(json.dumps(result))
 
 
 @csrf_exempt
 def write(request, path):
     data = request.POST.get('data')
     result = fs.write(path, data)
-    return JsonHttpResponse(result, 'object')
+    return HttpResponse(json.dumps(result))
 
 
 @csrf_exempt
 def execute(request, path):
     result = fs.execute(path)
-    return JsonHttpResponse(result)
+    return HttpResponse(json.dumps(result))
 
 
 @csrf_exempt
 def delete(request, path):
     print('delete')
     result = fs.delete(path)
-    return JsonHttpResponse(result)
+    return HttpResponse(json.dumps(result))

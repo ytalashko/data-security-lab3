b2f41a8a65cf3740e0cbf56ee33833a2c3b6cc1f
Eduard Pashchuk <ed.pashchuk@gmail.com>
2015-10-27 00:11:50 +0200

    restyling index.html

--- data-security-lab3/static/js/login.js
+++ data-security-lab3/static/js/login.js
@@ -15,19 +15,22 @@ var getCaptcha = function(succes){
 $(function(){
   $('#loginForm').submit(function(){
     $.post($(this).attr('action'), $(this).serialize(), function(json) {
-      if(json === true){
-          window.location.href = 'read/';
-          setInterval(function(){
-              getCaptcha(function(captcha){
-                  var captchaText = 'please enter captcha: F(' + captcha + ') = ?';
-                  var result = prompt(captchaText);
-                  var res = checkCaptcha(result);
-                  if(res !== true) {
-                      window.location.href = '/'
-                  }
-              })
-          },5000);
-      }
+        if (json !== true) {
+            alert('wrong data');
+            $('#loginForm').find('input[type=text], input[type=password], textarea').val('');
+        } else {
+            window.location.href = 'read/';
+            setInterval(function () {
+                getCaptcha(function (captcha) {
+                    var captchaText = 'please enter captcha: F(' + captcha + ') = ?';
+                    var result = prompt(captchaText);
+                    var res = checkCaptcha(result);
+                    if (res !== true) {
+                        window.location.href = '/'
+                    }
+                })
+            }, 5000);
+        }
     }, 'json');
     return false;
   });

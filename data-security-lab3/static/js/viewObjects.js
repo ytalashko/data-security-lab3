44013c65449109550adc8faaa9a33175de7c0fb8
Eduard Pashchuk <ed.pashchuk@gmail.com>
2015-11-01 00:59:24 +0200

    [frontend] add icons

--- data-security-lab3/static/js/viewObjects.js
+++ data-security-lab3/static/js/viewObjects.js
@@ -1,4 +1,7 @@
+//todo: improve color map
 var rowClasses = ['#00CC99', '#74FFFF'];
+
+var path = '';
 
 $('document').ready(function(){
     readData('').success(generateTable);
@@ -15,8 +18,11 @@ var generateTable = function(json){
     var data = JSON.parse(json);
     var table = $('#table tbody');
     for(var i in data){
+        var isFile = data[i]['type']==='f';
         var row = $('<tr></tr>').attr('bgcolor',rowClasses[i%2]);
-        row.append($('<td></td>').addClass('col-md-1').text(data[i]['type']));
+        row.append($('<td></td>').append( $('<span></span>').addClass('glyphicon')
+                .addClass(isFile?'glyphicon-file':'glyphicon-folder-open')
+        ).addClass('col-md-1'));
         row.append($('<td></td>').text(data[i]['name']));
         row.append($('<td></td>').text(data[i]['user']));
         row.append($('<td></td>').text(data[i]['right']));

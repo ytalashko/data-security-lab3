c9d43bedf21c11809ba88036f4934b12458592e5
vitaliy <vitalitylevitskiand@gmail.com>
2015-11-03 18:05:51 +0200

    fix error with missing csrf token

--- data-security-lab3/views.py
+++ data-security-lab3/views.py
@@ -1,11 +1,14 @@
 from django.http import HttpResponse
 from django.template import loader
+from django.views.decorators.csrf import csrf_exempt
 
 
+@csrf_exempt
 def index(request):
     template = loader.get_template('index.html')
     return HttpResponse(template.render())
 
+@csrf_exempt
 def viewObjects(request):
     template = loader.get_template('viewObjects.html')
     return HttpResponse(template.render())
